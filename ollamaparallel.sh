9c6b16a84126ba4af5c99f34ce0810dce7e77970
Sasha <67932607+placebeyondtheclouds@users.noreply.github.com>
2024-09-05 16:24:39 +0800

    ollama

--- ollamaparallel.sh
+++ ollamaparallel.sh
@@ -5,7 +5,7 @@ INSTANCES_PER_GPU=5
 PORT_START=11435
 
 for current_gpu_number in $(seq 0 $((GPUS - 1))); do
-    for process_number in $(seq 0 $((INSTANCES_PER_GPU - 1))); do
+    for process_number in $(seq 1 $((INSTANCES_PER_GPU))); do
         current_port=$((PORT_START + process_number))
         CUDA_VISIBLE_DEVICES=$current_gpu_number OLLAMA_HOST=127.0.0.1:$current_port ./ollama serve &
         echo "started process $process_number on gpu $current_gpu_number on port $current_port"
