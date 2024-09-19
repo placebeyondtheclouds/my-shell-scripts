6a6d751e99e16c9be6c59504bd898110ce269e3b
Sasha <67932607+placebeyondtheclouds@users.noreply.github.com>
2024-09-19 09:26:25 +0800

    edits

--- multipart-targz-to-tar.sh
+++ multipart-targz-to-tar.sh
@@ -4,5 +4,5 @@ ARCHIVE_DESTINATION=${1:-.}
 
 for base in $(ls *.tar.gz.* | sed 's/\.[^.]*$//' | sort -u); do
     echo "转换ing all parts of $base"
-    zcat ${base}.* >$ARCHIVE_DESTINATION/${base%.tar.gz}.tar
+    cat ${base}.* | zcat >$ARCHIVE_DESTINATION/${base%.tar.gz}.tar
 done
