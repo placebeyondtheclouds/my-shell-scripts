f553d361f1c3dc0e2b93dbfe0322f8f3265ff7b2
Sasha <67932607+placebeyondtheclouds@users.noreply.github.com>
2024-09-19 15:56:26 +0800

    edits

--- mount-tars.sh
+++ mount-tars.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 controlc() {
     exit 1
@@ -7,6 +7,17 @@ controlc() {
 SOURCE_ARCHIVES=$1
 MOUNT_DESTINATION=$2
 SKIPKEYWORD=".ipynb_checkpoints"
+
+cat <<"EOF"
+                                     __                                     ______  ______   ____               
+ /'\_/`\                           /\ \__                                 /\__  _\/\  _  \ /\  _`\             
+/\      \     ___    __  __    ___ \ \ ,_\        ___    __  __   _ __    \/_/\ \/\ \ \L\ \\ \ \L\ \     ____  
+\ \ \__\ \   / __`\ /\ \/\ \ /' _ `\\ \ \/       / __`\ /\ \/\ \ /\`'__\     \ \ \ \ \  __ \\ \ ,  /    /',__\ 
+ \ \ \_/\ \ /\ \L\ \\ \ \_\ \/\ \/\ \\ \ \_     /\ \L\ \\ \ \_\ \\ \ \/       \ \ \ \ \ \/\ \\ \ \\ \  /\__, `\
+  \ \_\\ \_\\ \____/ \ \____/\ \_\ \_\\ \__\    \ \____/ \ \____/ \ \_\        \ \_\ \ \_\ \_\\ \_\ \_\\/\____/
+   \/_/ \/_/ \/___/   \/___/  \/_/\/_/ \/__/     \/___/   \/___/   \/_/         \/_/  \/_/\/_/ \/_/\/ / \/___/ 
+  
+EOF
 
 #checks
 if [ -z "$SOURCE_ARCHIVES" ] || [ -z "$MOUNT_DESTINATION" ]; then
@@ -28,11 +39,20 @@ if [ ! -d "$SOURCE_ARCHIVES" ]; then
 fi
 
 #list archives
+# generate a separator
+
 declare -a archivefiles
 IFS=$'\n'
 for line in $(find "$SOURCE_ARCHIVES" -type f -name "*.tar" 2>/dev/null | sort -n); do
     archivefiles+=("$line")
 done
+cat <<"EOF"
+ _____  _    ___        __                      _  _ 
+|_   _|/_\  | _ \ ___  / _| ___  _  _  _ _   __| |(_)
+  | | / _ \ |   /(_-< |  _|/ _ \| || || ' \ / _` | _ 
+  |_|/_/ \_\|_|_\/__/ |_|  \___/ \_,_||_||_|\__,_|(_)
+                                                                                
+EOF
 echo "archive count: ${#archivefiles[@]}"
 echo "${archivefiles[@]}"
 
@@ -43,6 +63,13 @@ for line in $(find "$MOUNT_DESTINATION" -maxdepth 1 -type d 2>/dev/null | grep -
     alldirs+=("$line")
 done
 alldirs=("${alldirs[@]:1}")
+cat <<"EOF"
+                        _          __                      _  _ 
+ _ __   ___  _  _  _ _ | |_  ___  / _| ___  _  _  _ _   __| |(_)
+| '  \ / _ \| || || ' \|  _|(_-< |  _|/ _ \| || || ' \ / _` | _ 
+|_|_|_|\___/ \_,_||_||_|\__|/__/ |_|  \___/ \_,_||_||_|\__,_|(_)
+                                                                                                                                     
+EOF
 echo "mount points to unmount: ${#alldirs[@]}"
 echo "${alldirs[@]}"
 
