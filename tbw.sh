046effc65809b43c9b0f4557bb63e06d647dcee1
Sasha <67932607+placebeyondtheclouds@users.noreply.github.com>
2024-10-29 15:00:48 +0800

    corrected for Intel drives

--- tbw.sh
+++ tbw.sh
@@ -2,41 +2,39 @@
 # smart parameters references https://www.backblaze.com/blog/making-sense-of-ssd-smart-stats/
 
 for drive in /dev/sd[a-z]; do
+    model=$(sudo smartctl -a $drive | awk -F: '/Model Family/ {print $2}' | xargs)
 
-    #for INTEL:
-    if sudo smartctl -a $drive | grep -q "Model Family:     Intel"; then
+    case "$model" in
+    *Intel*)
         onewrite=32768
         sudo smartctl --attributes $drive | awk -v devname=$drive -v onewrite=$onewrite '
-    /(241)/ {
-      B=$10 * onewrite;
-      printf("%s: Attribute %d, Intel: %.2f TiB \n", devname, $1, B/1024^4, onewrite)
-    }'
-        continue
-    fi
+            /(241)/ {
+              B=$10 * onewrite;
+              printf("%s: Attribute %d, Intel: %.2f TiB \n", devname, $1, B/1024^4, onewrite)
+            }'
+        ;;
+    #any other vendor
+    *)
 
-    #for Samsung and Crucial:
-    pss=$(
-        sudo smartctl -a $drive | awk '
-        /Sector Sizes|Sector Size/ {
-            if ($0 ~ /logical\/physical/) {
-                split($0, a, /[ ,/]/);
-                for (i in a) if (a[i] == "physical") {print a[i-3]; break}
-            } else if ($0 ~ /physical/) {
-                split($0, a, /[ ,/]/);
-                for (i in a) if (a[i] == "physical") {print a[i-2]; break}
-            }
-        }'
-    )
+        pss=$(
+            sudo smartctl -a $drive | awk '
+                /Sector Sizes|Sector Size/ {
+                    if ($0 ~ /logical\/physical/) {
+                        split($0, a, /[ ,/]/);
+                        for (i in a) if (a[i] == "physical") {print a[i-3]; break}
+                    } else if ($0 ~ /physical/) {
+                        split($0, a, /[ ,/]/);
+                        for (i in a) if (a[i] == "physical") {print a[i-2]; break}
+                    }
+                }'
+        )
 
-    # Check if pss is empty or zero
-    if [[ -z "$pss" || "$pss" == "bytes" || "$pss" -eq 0 ]]; then
-        echo "Error: Failed to get physical sector size for $drive"
-        continue
-    fi
+        sudo smartctl --attributes $drive | awk -v devname=$drive -v pss=$pss '
+            /(241|246)/ {
+              B=$10 * pss;
+              printf("%s: Attribute %d: %.2f TiB (Physical Sector Size: %d bytes)\n", devname, $1, B/1024^4, pss)
+            }'
+        ;;
 
-    sudo smartctl --attributes $drive | awk -v devname=$drive -v pss=$pss '
-    /(241|246)/ {
-      B=$10 * pss;
-      printf("%s: Attribute %d: %.2f TiB (Physical Sector Size: %d bytes)\n", devname, $1, B/1024^4, pss)
-    }'
+    esac
 done
